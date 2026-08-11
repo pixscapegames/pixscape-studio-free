791f780d7802a248f32443037e0c5e4e849396e3
Laurent Robbens <lau.robbens@gmail.com>
2026-08-11 21:13:10 +0200

    fix(gwt): include prefab runtime types in reflection

--- src/main/resources/html-preview-template/htmlplayer/htmlplayer.nocache.js
+++ src/main/resources/html-preview-template/htmlplayer/htmlplayer.nocache.js
@@ -342,7 +342,7 @@ function htmlplayer(){
     }
     var strongName;
     try {
-      strongName = '9E39DF7533705E94230661E86B598269';
+      strongName = '87B3A2627CF1898524B11F83C559CF82';
       var idx = strongName.indexOf(':');
       if (idx != -1) {
         softPermutationId = parseInt(strongName.substring(idx + 1), 10);
